46c3f91643b9bbc0665e9712be0ae7a4afb816d1
Zentyl <matszc32@wp.pl>
2024-05-29 17:58:49 +0200

    Added lilies

--- script.js
+++ script.js
@@ -5,69 +5,64 @@ window.onload = () => { // uruchomienie gry przy załadowaniu okna strony
 class Game { // klasa gry
     backgrounds = []
     gameSpeed = 1;
-    carSpeed = 2;
+    lilySpeed = 20;
     trainSpeed = 20;
-    cars = [];
+    carSpeed = 2;
+    lilies = [];
     trains = [];
+    cars = [];
+
 
     init = () => { // konstruktor
         this.canvas = document.querySelector("canvas");
         this.ctx = this.canvas.getContext("2d");
+
         this.canvas.width = 900;
         this.canvas.height = 565;
 
         this.background = new Image();
         this.background.src = "img/background.png"; // tło
 
-        this.player = new Image();
-        this.player.src = "img/player/playerup.png"; // postać gracza
-        this.playerWidth = this.player.width * 0.5;
-        this.playerHeight = this.player.height * 0.5;
-        this.playerX = this.canvas.width / 2 - this.player.width;
-        this.playerY = this.canvas.height - this.player.height * 2 + 12;
+        this.playerImg = new Image();
+        this.playerImg.src = "img/player/playerup.png"; // postać gracza
 
-        this.carDown = new Image();
-        this.carDown.src = "img/cars/down.png"; // przeszkoda
-        this.carUp = new Image();
-        this.carUp.src = "img/cars/up.png"; // przeszkoda
-        this.carStreet = new Image();
-        this.carStreet.src = "img/cars/street.png";
-        this.train = new Image();
-        this.train.src = "img/train/riding.png";
-        this.trainIncoming = new Image();
-        this.trainIncoming.src = "img/train/incoming.png";
-        this.trainTrack = new Image();
-        this.trainTrack.src = "img/train/track.png";
-        this.trainAlert = new Image();
-        this.trainAlert.src = "img/train/alert.png";
+        this.lilyRiverImg = new Image();
+        this.lilyRiverImg.src = "img/lily/river.png";
+        this.lilyFloatingImg = new Image();
+        this.lilyFloatingImg.src = "img/lily/floating.png";
+        this.lilySunkenImg = new Image();
+        this.lilySunkenImg.src = "img/lily/sunken.png";
+        this.trainRidingImg = new Image();
+        this.trainRidingImg.src = "img/train/riding.png";
+        this.trainIncomingImg = new Image();
+        this.trainIncomingImg.src = "img/train/incoming.png";
+        this.trainTrackImg = new Image();
+        this.trainTrackImg.src = "img/train/track.png";
+        this.trainAlertImg = new Image();
+        this.trainAlertImg.src = "img/train/alert.png";
+        this.carDownImg = new Image();
+        this.carDownImg.src = "img/cars/down.png"; // przeszkoda
+        this.carUpImg = new Image();
+        this.carUpImg.src = "img/cars/up.png"; // przeszkoda
+        this.carStreetImg = new Image();
+        this.carStreetImg.src = "img/cars/street.png";
 
-        let x1 = 0;
-        let y1 = this.canvas.height - this.background.height;
+        this.player = {
+            width: this.playerImg.width * 0.5,
+            height: this.playerImg.height * 0.5,
+            x: this.canvas.width / 2 - this.playerImg.width,
+            y: this.canvas.height - this.playerImg.height * 2 + 12
+        };
+
 
         this.startGame();
     };
 
-    drawBoard = () => {
-        let bw = this.canvas.width;
-        let bh = this.canvas.height;
-        let p = 0;
-        for (let x = 0; x <= bw; x += 64) {
-            this.ctx.moveTo(0.5 + x + p, p);
-            this.ctx.lineTo(0.5 + x + p, bh + p);
-        }
-
-        for (let x = 0; x <= bh; x += 64) {
-            this.ctx.moveTo(p, 0.5 + x + p);
-            this.ctx.lineTo(bw + p, 0.5 + x + p);
-        }
-        this.ctx.strokeStyle = "white";
-        this.ctx.stroke();
-    }
-
     createRandomArray = () => {
         this.randomObstacles = [
-            this.addCars,
+            this.addLilies,
             this.addTrains,
+            this.addCars
         ];
     }
 
@@ -85,20 +80,20 @@ class Game { // klasa gry
 
                 switch (e.key) {
                     case "ArrowDown":
-                        this.playerY += 64;
-                        this.player.src = "img/player/playerdown.png";
+                        this.player.y += 64;
+                        this.playerImg.src = "img/player/playerdown.png";
                         break;
                     case "ArrowUp":
-                        this.playerY -= 64;
-                        this.player.src = "img/player/playerup.png";
+                        this.player.y -= 64;
+                        this.playerImg.src = "img/player/playerup.png";
                         break;
                     case "ArrowLeft":
-                        this.playerX -= 64;
-                        this.player.src = "img/player/playerleft.png";
+                        this.player.x -= 64;
+                        this.playerImg.src = "img/player/playerleft.png";
                         break;
                     case "ArrowRight":
-                        this.playerX += 64;
-                        this.player.src = "img/player/playerright.png";
+                        this.player.x += 64;
+                        this.playerImg.src = "img/player/playerright.png";
                         break;
                     case "Enter":
                         this.restartGame();
@@ -133,27 +128,29 @@ class Game { // klasa gry
             }
         }
         update();
+
         this.addBackgrounds();
         this.createRandomArray();
         this.getRandomObstacle();
+
     };
 
     updateGame = () => { // aktualizowanie gry
-            this.drawBackgrounds();
-            this.drawCars();
-            this.drawTrains();
-            this.drawPlayer();
-            this.checkCollision();
-            this.checkMove();
-            this.ctx.fillStyle = "white";
-            this.ctx.font = "20px Verdana";
-            this.ctx.fillText("Score: " + this.score, 80, 55);
+        this.drawBackgrounds();
+        this.drawLilies();
+        this.drawTrains();
+        this.drawCars();
+        this.drawPlayer();
+        this.checkMove();
+        this.ctx.fillStyle = "white";
+        this.ctx.font = "20px Verdana";
+        this.ctx.fillText("Score: " + this.score, 80, 55);
     };
 
     drawPlayer = () => { // rysowanie postaci gracza oraz jej fizyka
         // rysowanie postaci gracza
-        this.playerY += this.gameSpeed;
-        this.ctx.drawImage(this.player, this.playerX, this.playerY);
+        this.player.y += this.gameSpeed;
+        this.ctx.drawImage(this.playerImg, this.player.x, this.player.y);
     };
 
     addBackgrounds = () => { // tworzenie tła
@@ -179,7 +176,6 @@ class Game { // klasa gry
             if (background.y == 0) {
                 this.backgrounds.shift(); // usuwanie tła z tablicy
                 this.addBackgrounds(); // dodawanie tła do tablicy
-
             }
 
             if (background.y % 320 == 0) {
@@ -188,38 +184,112 @@ class Game { // klasa gry
         });
     };
 
+    addLilies = () => {
+        const possibleX2 = [
+            320, 384, 448, 512, 576
+        ]
+        let randomPosition = 0;
+        let x1 = 0;
+        let y = 0;
+        let x2 = 0;
+        do {
+            do {
+                randomPosition = (Math.floor(Math.random() * 14 / 2) * 2);
+            } while (randomPosition == 0)
+            x1 = this.canvas.width - this.lilyFloatingImg.width * randomPosition;
+            y = 64 * (-1);
+            x2 = x1 - possibleX2[Math.floor(Math.random() * possibleX2.length)];
+            if (x2 <= 0) {
+                x2 *= (-1);
+            }
+        } while (
+            x1 + 56 == x2 || x1 - 8 == x2 || x1 - 72 == x2 || x1 + 120 == x2 || x1 - 200 == x2
+            || x1 - 136 == x2 || x1 - 264 == x2 || x1 + 248 == x2 || x1 + 184 == x2 // niepożądane lokalizacje lilii
+        )
+        console.log("x1: " + x1);
+        console.log("x2: " + x2);
+        this.lilies.push({ // tablica z przeszkodami
+            floating: {
+                img: this.lilyFloatingImg,
+                x1: x1,
+                y: y,
+
+                x2: x2,
+                width: this.lilyFloatingImg.width,
+                height: this.lilyFloatingImg.height
+            },
+            river: {
+                img: this.lilyRiverImg,
+                x1: x1,
+                y: y,
+                x2: x2,
+                width: this.lilyRiverImg.width,
+                height: this.lilyRiverImg.height
+            },
+        });
+    }
+
+    drawLilies = () => { // rysowanie przeszkód
+        const liliesToDraw = [...this.lilies]; // przeszkody oczekujace na narysowanie
+        liliesToDraw.forEach(lily => {
+            this.ctx.drawImage(lily.river.img, 0, lily.floating.y); // prawa
+            this.ctx.drawImage(lily.floating.img, lily.floating.x1, lily.floating.y);
+            this.ctx.drawImage(lily.floating.img, lily.floating.x2, lily.floating.y);
+
+            lily.floating.y += this.gameSpeed;
+
+            if (
+                !((this.player.x == lily.floating.x1 - 2 || this.player.x == lily.floating.x2 - 2))
+                && (this.player.y == lily.floating.y - 2)
+            ) {
+                console.log("ŚMIERĆ RZEKA");
+                this.restartGame();
+            }
+
+            if (lily.floating.y == this.canvas.height + lily.floating.height) {
+                this.lilys.shift();
+            }
+
+            if (this.player.y > lily.floating.y - 64 && this.player.y < lily.floating.y + 62) {
+                this.player.y = lily.floating.y - 2;
+            }
+        });
+
+    };
+
+
     addTrains = () => { // tworzenie przeszkod
-        let x = this.canvas.width - this.carDown.width;
+        let x = this.canvas.width - this.carDownImg.width;
         let y = 64 * (-1);
 
         this.trains.push({ // tablica z przeszkodami
             riding: {
-                img: this.train,
+                img: this.trainRidingImg,
                 x: x,
                 y: y,
-                width: this.train.width,
-                height: this.train.height
+                width: this.trainRidingImg.width,
+                height: this.trainRidingImg.height
             },
             incoming: {
-                img: this.trainIncoming,
+                img: this.trainIncomingImg,
                 x: x,
                 y: y,
-                width: this.trainIncoming.width,
-                height: this.trainIncoming.height
+                width: this.trainIncomingImg.width,
+                height: this.trainIncomingImg.height
             },
             track: {
-                img: this.trainTrack,
+                img: this.trainTrackImg,
                 x: x,
                 y: y,
-                width: this.trainTrack.width,
-                height: this.trainTrack.height
+                width: this.trainTrackImg.width,
+                height: this.trainTrackImg.height
             },
             alert: {
-                img: this.trainAlert,
+                img: this.trainAlertImg,
                 x: x,
                 y: y,
-                width: this.trainAlert.width,
-                height: this.trainAlert.height
+                width: this.trainAlertImg.width,
+                height: this.trainAlertImg.height
             },
             spawn: Math.random() * (320 - 128) + 128
         });
@@ -243,10 +313,10 @@ class Game { // klasa gry
                     train.riding.x -= this.trainSpeed;
 
                     if (
-                        
-                        ((this.playerX + this.playerWidth >= train.riding.x) && (this.playerX < train.riding.x + train.riding.width))
-                        && ((this.playerY >= train.track.y) && (this.playerY + 2 < train.track.y + train.riding.height))
-                    
+
+                        ((this.player.x + this.player.width >= train.riding.x) && (this.player.x < train.riding.x + train.riding.width))
+                        && ((this.player.y >= train.track.y) && (this.player.y + 2 < train.track.y + train.riding.height))
+
                     ) {
                         console.log("ŚMIERĆ POCIĄG");
                         this.restartGame();
@@ -256,38 +326,38 @@ class Game { // klasa gry
             if (train.track.y == this.canvas.height + train.riding.height) {
                 this.trains.shift(); // usuwanie przeszkód z tablicy
             }
-            if (this.playerY > train.track.y - 64 && this.playerY < train.track.y + 64) { // jeżeli postać jest blisko pola pociągu ale nie jest w calosci to go przeteleportuj na pole
-                this.playerY = train.track.y;
+            if (this.player.y > train.track.y - 64 && this.player.y < train.track.y + 64) { // jeżeli postać jest blisko pola pociągu ale nie jest w calosci to go przeteleportuj na pole
+                this.player.y = train.track.y;
             }
 
         });
     };
 
     addCars = () => { // tworzenie przeszkod
-        let x = this.canvas.width - this.carDown.width;
+        let x = this.canvas.width - this.carDownImg.width;
         let y = 64 * (-1);
 
         this.cars.push({ // tablica z przeszkodami
             up: {
-                img: this.carUp,
+                img: this.carUpImg,
                 x: x - Math.random() * (100 - (-100)) + (-100),
                 y: y,
-                width: this.carDown.width,
-                height: this.carDown.height
+                width: this.carDownImg.width,
+                height: this.carDownImg.height
             },
             down: {
-                img: this.carDown,
+                img: this.carDownImg,
                 x: Math.random() * (200 - (-200)) + (-200),
                 y: y,
-                width: this.carUp.width,
-                height: this.carUp.height
+                width: this.carUpImg.width,
+                height: this.carUpImg.height
             },
             street: {
-                img: this.carStreet,
+                img: this.carStreetImg,
                 x: x,
                 y: y,
-                width: this.carStreet.width,
-                height: this.carStreet.height
+                width: this.carStreetImg.width,
+                height: this.carStreetImg.height
             }
         });
     };
@@ -304,18 +374,16 @@ class Game { // klasa gry
             car.down.y += this.gameSpeed;
             car.up.x -= this.carSpeed;
             car.up.y += this.gameSpeed;
-            console.log(this.playerY + 64);
-            console.log("car: " + car.up.y);
             if (
-                (this.playerX + this.playerWidth >= car.down.x && this.playerX < car.down.x + car.down.width)
-                && ((this.playerY == car.down.y - 2))
+                (this.player.x + this.player.width >= car.down.x && this.player.x < car.down.x + car.down.width)
+                && ((this.player.y == car.down.y))
             ) {
                 console.log("ŚMIERĆ DOWN");
                 this.restartGame();
             }
             if (
-                ((this.playerX + this.playerWidth*2 >= car.up.x) && (this.playerX < car.up.x + car.up.width))
-                && ((this.playerY  == car.up.y - 66))
+                ((this.player.x + this.player.width * 2 >= car.up.x) && (this.player.x < car.up.x + car.up.width))
+                && ((this.player.y == car.up.y - 64))
             ) {
                 console.log("ŚMIERĆ TOP"); // trzeba zrobic Y
                 this.restartGame();
@@ -325,36 +393,10 @@ class Game { // klasa gry
                 this.cars.shift();
             }
 
-            if (this.playerY > car.down.y - 64 && this.playerY < car.down.y + 2) {
-                this.playerY = car.down.y - 2;
+            if (this.player.y > car.down.y - 56 && this.player.y < car.down.y + 6) {
+                this.player.y = car.down.y;
             }
-            // if(this.playerY > car.down.y - 64 && this.playerY < car.down.y + 64) {
-            //     this.playerY = car.up.y;
-            // }
         });
-    };
-
-    checkCollision = () => { // sprawdzanie kolizji przeszkody z graczem
-        const carsToCheck = [...this.cars]; // przeszkody oczekujace na sprawdzenie kolizji
-        carsToCheck.forEach(car => {
-            if (car.x == this.playerX) {
-                this.score++; // jeśli gracz przeskoczył przeszkodę dodaj punkt
-                if (this.score % 15 == 0) { // zwiększ prędkość ruchu przeszkód gdy ilość zdobytych punktow to wielokrotność 15
-                    if (this.gameSpeed == 15) {
-                        this.gameSpeed = 20;
-                    }
-                    else {
-                        this.gameSpeed += 4;
-                    }
-
-                }
-            }
-            // warunek sprawdzajacy czy przeszkoda nie dotknęła gracza
-
-            this.isOver = false;
-
-
-        })
     };
 
     clearCanvas = () => { // czyszczenie canvy
@@ -364,10 +406,11 @@ class Game { // klasa gry
 
     restartGame = () => { // restartowanie gry
         this.score = 0;
-        this.playerY = this.canvas.height - this.player.height * 2 + 12;
+        this.player.y = this.canvas.height - this.playerImg.height * 2 + 12;
         this.backgrounds = [];
-        this.cars = [];
+        this.lilies = [];
         this.trains = [];
+        this.cars = [];
         this.addBackgrounds();
         this.getRandomObstacle();
         this.gameSpeed = 2;
